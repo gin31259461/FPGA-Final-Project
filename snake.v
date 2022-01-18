77ba5b2642468af982ab9fc2deede2a5e0ab9d79
Abner <86829910+gin31259461@users.noreply.github.com>
2022-01-18 15:37:13 +0800

    fix bug

--- snake.v
+++ snake.v
@@ -40,16 +40,28 @@ module snake(
 	 8'b11111111
   };             //matrix x, y start position : row=x, col=y
   
+  logic[7:0] food[7:0] =
+  '{
+    8'b11111111, //x, y start position
+	 8'b11111111,
+	 8'b11111111,
+	 8'b11111111,
+	 8'b11111111,
+	 8'b11111111,
+	 8'b11111111,
+	 8'b11111111
+  };
+  
     logic[7:0] game_over[7:0] =
   '{
-    8'b10101010,
-	 8'b01010101,
-	 8'b10101010,
-	 8'b01010101,
-	 8'b10101010,
-	 8'b01010101,
-	 8'b10101010,
-	 8'b01010101
+    8'b01111110,
+	 8'b10111101,
+	 8'b11011011,
+	 8'b11100111,
+	 8'b11100111,
+	 8'b11011011,
+	 8'b10111101,
+	 8'b01111110
   };
   
 
@@ -65,6 +77,7 @@ module snake(
   int snake_body[29:0][1:0];
   int snake_body_index;
   int check_status, game_over_status;
+  
   
   int i;
   
@@ -139,7 +152,7 @@ module snake(
   
     if(game_over_status != 1) begin
 	 
-      generate_food(rand_x[rand_index], rand_y[rand_index], board, board);
+      generate_food(rand_x[rand_index], rand_y[rand_index], food, food);
 	   food_xor = rand_x[rand_index];
 	   food_yor = rand_y[rand_index];
 	 
@@ -169,13 +182,13 @@ module snake(
 		if(snake_head_x == food_xor && snake_head_y == food_yor) begin
         rand_index += 1;
 		  score += 1'b1;
+		  check_status = 1;
 		  
 		  if(snake_body_index != 29) begin
 		    snake_body_index += 1;
-		    check_status = 1;
 		  end
 	   end
-		
+		 
 		if(score == 4'b1010) begin
 		  score = 4'b0000;
 		  tens_score += 1'b1;
@@ -186,14 +199,15 @@ module snake(
     	if(rand_index > 31)
 	     rand_index = 0;
 
+		board[snake_head_x][snake_head_y] = 1'b0;
       if(check_status == 1) begin
 		  snake_body[snake_body_index][0] = current_x;
         snake_body[snake_body_index][1] = current_y;
+		  food[snake_head_x][snake_head_y] = 1'b1;
 		  check_status = 0;
 		end
 		
 		else if(check_status == 0) begin
-		  board[snake_head_x][snake_head_y] = 1'b0;
 		  board[snake_body[0][0]][snake_body[0][1]] = 1'b1;
 		  i = 0;
 		  repeat(29) begin
@@ -240,10 +254,13 @@ module snake(
 	 
 	 if(game_over_status == 1) begin
 	   DATA_G = 8'b11111111;
+		DATA_B = 8'b11111111;
 	   DATA_R = game_over[SEL[2:0]];
 	 end
-	 else
+	 else begin
 	   DATA_G = board[SEL[2:0]];
+		DATA_B = food[SEL[2:0]];
+	 end
   end
   
   always @(speed_level)
